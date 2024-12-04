942a5c34e5c3f0147c24080d5614e001f87025f2
Abinash Bhatta <abinash.bhatta@toradex.com>
2024-12-04 09:38:06 +0530

    added the custome fields and preventing from creating the new organization

--- lib/discourse_zendesk_plugin/helper.rb
+++ lib/discourse_zendesk_plugin/helper.rb
@@ -99,12 +99,13 @@ module DiscourseZendeskPlugin
       result = zendesk_client.users.search(query: user.email)
       return result.first if result.present? && result.size == 1
       custom_fields = UserCustomField.where(user_id: user.id).pluck(:name, :value).to_h
-      organization_name = custom_fields['user_field_1'] || "NA"
-      organization = fetch_organization(organization_name)
+      # organization_name = custom_fields['user_field_1'] || "NA"
+      # organization = fetch_organization(organization_name)
       user_fields = {
         job_function: custom_fields['user_field_3'] || "NA",
         country: custom_fields['user_field_2'] || "NA",
         title: user.name || "NA",
+        organization_from_community:custom_fields['user_field_1'] || "NA"
         # department: custom_fields['user_field_1'] || "NA"
       }
       zendesk_client.users.create(
@@ -112,7 +113,7 @@ module DiscourseZendeskPlugin
         email: user.email,
         verified: true,
         role: "end-user",
-        organization_id: organization.id,
+        # organization_id: organization.id,
         user_fields: user_fields
       )
     end
